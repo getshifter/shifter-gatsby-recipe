04e4ec560c894392aeb0ecf8ced12b581bcc10a7
haruharuharuby <cocktail.house@gmail.com>
2020-07-04 15:49:25 +0200

    add gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+{
+    resolve: 'gatsby-source-graphql',
+    options: {
+        typeName: 'WORDPRESS',
+        fieldName: 'wordpress',
+        url: 'https://cms.digipresence.org/admintuts/graphql',
+    }
+}
