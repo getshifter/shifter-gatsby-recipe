012b2b49a4f34c15c55f863d06daa08065494aa5
haruharuharuby <cocktail.house@gmail.com>
2020-07-05 16:37:04 +0200

    wrong structure of gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    resolve: 'gatsby-source-graphql',
-    options: {
-        typeName: 'WORDPRESS',
-        fieldName: 'wordpress',
-        url: 'https://cms.digipresence.org/admintuts/graphql',
-    }
+    plugins: [
+        {
+            resolve: 'gatsby-source-graphql',
+            options: {
+                typeName: 'WORDPRESS',
+                fieldName: 'wordpress',
+                url: 'https://cms.digipresence.org/admintuts/graphql',
+            }
+        }
+    ]
 }
