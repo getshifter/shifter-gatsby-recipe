44a610b0b495dafcb097daa97c896da51f80b1ad
haruharuharuby <cocktail.house@gmail.com>
2020-07-06 13:09:49 +0200

    index.js

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,31 +1,83 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import '../styles/category.css'
+import { graphql, Link } from 'gatsby'
+import '../styles/index.css'
 
-const CategoryTemplate = ({data}) => {
-    const { name, count, description } = data.wordpress.category
+function Pages(props) {
+  const items = props.items
+  const listItems = items.map(item => {
+    return (
+      <li key={item.id}>
+        <Link to={item.uri}>{item.title}</Link>
+      </li>
+    )
+  })
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  )
+}
 
+function Posts(props) {
+  const items = props.items
+  const listItems = items.map(item => {
+    return (
+      <li key={item.id}>
+        <Link to={`/blog/${item.slug}`}>{item.title}</Link>
+      </li>
+    )
+  })
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  )
+}
+
+const IndexTemplate = ({data}) => {
+    const posts = data.wordpress.posts.nodes
+    const pages = data.wordpress.pages.nodes
+
+    console.log(posts)
+    console.log(pages)
     return <>
-        <div className="category">
-            <h2 dangerouslySetInnerHTML={{ __html: name }}></h2>
-            <span className="cat-count">({count})</span>
+        <div className="pages">
+            <h2 dangerouslySetInnerHTML={{ __html: 'pages' }}></h2>
+            <div>
+              <Pages items={pages} />
+            </div>
         </div>
-        <div>
-            <p>{description}</p>
+        <div className="posts">
+            <h2 dangerouslySetInnerHTML={{ __html: 'posts' }}></h2>
+            <div>
+              <Posts items={posts} />
+            </div>
         </div>
     </>
 } 
 
 export const query = graphql`
-  query CategoryQuery($id: ID!) {
+  query IndexQuery {
     wordpress {
-      category(id: $id, idType: DATABASE_ID) {
-        name
-        count
-        description
+      posts {
+        nodes {
+          id
+          title
+          slug
+          link
+          uri
+        }
+      }
+      pages {
+        nodes {
+          id
+          title
+          slug
+          uri
+        }
       }
     }
   }
 `
 
-export default CategoryTemplate
+export default IndexTemplate
