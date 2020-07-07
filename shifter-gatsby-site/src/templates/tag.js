c36b05474abd7f959912ce4b03ea501d039effed
Daniel Olson <emaildano@gmail.com>
2020-07-07 17:23:29 -0400

    Revised repo to work with REST

---
+++ shifter-gatsby-site/src/templates/tag.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+const TagTemplate = ({ data }) => {
+  const { name, description, count } = data.wordpressTag
+
+  return (
+    <>
+      <div className="tag" style={{ display: "flex", flexDirection: "column" }}>
+        Tag Name: <h1 dangerouslySetInnerHTML={{ __html: name }}></h1>
+        Tag Count: <div class="tag-count">({count})</div>
+        Tag Description:{" "}
+        <p dangerouslySetInnerHTML={{ __html: description }}></p>
+      </div>
+    </>
+  )
+}
+
+export const query = graphql`
+  query TagQuery($id: Int!) {
+    wordpressTag(wordpress_id: { eq: $id }) {
+      id
+      description
+      count
+      slug
+      wordpress_id
+      name
+    }
+  }
+`
+
+export default TagTemplate
